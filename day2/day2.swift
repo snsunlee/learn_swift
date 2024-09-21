ddf5ba2c122913ed275190612c7caf0e89eaba76
snsunlee <snsunlee123@gmail.com>
2024-09-21 22:27:34 +0800

    day2 finish

---
+++ day2/day2.swift
@@ -0,0 +1,32 @@
+//Day2
+//Boolean
+//
+let goodDogs = true
+let gameOver = false
+let isMultiple = 120.isMultiple(of: 3)
+var isAuthenticated = false
+print(isAuthenticated)
+print(!isAuthenticated)
+//反转
+isAuthenticated.toggle()
+print(isAuthenticated)
+
+
+//Join Strings together
+
+let people = "Haters"
+let action = "hate"
+let lyric = people + " honna " + action
+
+let nname = "lixinshang"
+let age = 28
+let message = "Hello , my name is \(name) , and I'm \(age) years old"
+print(message)
+
+print("5 X 5 is \(5*5)")
+
+
+//checkpoint1
+let celsius = 36.8
+let fahrenhit = celsius*9/5+32
+print("Celsius is \(celsius) , fahrenhit is \(fahrenhit)")
